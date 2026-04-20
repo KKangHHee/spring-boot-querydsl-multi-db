1f68a89380389a3bf9c6aa9a682738536c0dbca9
KKangHHee <skh8609@gmail.com>
2026-04-20 19:35:27 +0900

    feat: add Repository annotation
    이전 브랜치에서 각각의 Repository 클래스에 Repository annotation이 빠져서 등록했습니다.
    
    Refs #7

--- src/main/java/com/example/demo/domain/postOracle/repository/PostOracleRepository.java
+++ src/main/java/com/example/demo/domain/postOracle/repository/PostOracleRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.domain.postOracle.repository;
 
 import com.example.demo.domain.postOracle.entity.PostOracle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostOracleRepository extends JpaRepository<PostOracle, Long> {
 
 }
